ea9072781558d4f163e439842f8ce3fb62d35789
knifebolt <jbaugher@rocketmail.com>
2024-04-17 22:10:31 -0400

    Update RecipeManager.js

--- RecipeManager/js/RecipeManager.js
+++ RecipeManager/js/RecipeManager.js
@@ -1,241 +1,10 @@
-var Recipes = [
-
-	{
-		Name: "Manhattan",
-		Ingredients: [
-			"Whiskey - 2oz",
-			"Red Vermouth - 0.75oz",
-			"Bitters - 1 dash",
-		],
-		Garnish: "Olive",
-		Directions: "Pour all ingredients into shaker with ice cubes, stir, strain into glass",
-		Cherry: true,
-		Olive: false,
-		Glass: "Martini",
-		Ice: false,
-	},
-	
-	{
-		Name: "Daiquiri",
-		Ingredients: [
-			"White Rum - 1.5oz",
-			"Lime Juice - 1oz",
-			"Simple Syrup - 0.5oz",
-			"Lime slice"
-		],
-		Garnish: "Lime slice",
-		Directions: "Pour all ingredients into shaker with ice cubes, stir, strain into glass",
-		Cherry: false,
-		Olive: false,
-		Glass: "Martini",
-		Ice: false,
-	},
-	
-	{
-		Name: "Martini",
-		Ingredients: [
-			"Gin - 2oz",
-			"Dry Vermouth - 1oz",
-			"Bitters - 1 dash",
-		],
-		Garnish: "Olive",
-		Directions: "Pour all ingredients into shaker with ice cubes, stir, strain into glass",
-		Cherry: false,
-		Olive: true,
-		Glass: "Martini",
-		Ice: false,
-	},
-	
-	{
-		Name: "Gimlet",
-		Ingredients: [
-			"Gin - 2oz",
-			"Simple Syrup - 0.75oz",
-			"Lime Juice - 0.75 dash",
-		],
-		Garnish: "Lime slice",
-		Directions: "Pour all ingredients into shaker with ice cubes, stir, strain into glass",
-		Cherry: false,
-		Olive: false,
-		Glass: "Martini",
-		Ice: false,
-	},
-	
-	{
-		Name: "Cosmopolitan",
-		Ingredients: [
-			"Citrus Vodka - 1.5oz",
-			"Cranberry Juice - 1oz",
-			"Cointreau - 0.5oz",
-			"Lime Juice - 0.5oz",
-		],
-		Garnish: "Lime Slice",
-		Directions: "Pour all ingredients into mixing glass with ice cubes, shake, strain into glass",
-		Cherry: false,
-		Olive: false,
-		Glass: "Martini",
-		Ice: false,
-	},
-	{
-		Name: "Mojito",
-		Ingredients: [
-			"White Rum - 2oz",
-			"Lime Juice - 0.75oz",
-			"3 mint leaves",
-			"Sugar - 2 teaspoons",
-			"Soda water to fill",
-			"Lime slice"
-		],
-		Garnish: "Lime Slice",
-		Directions: "Muddle mint and sugar and lime juice in a shaking tin, add ice and rest of ingredients, shake to chill, strain into glass with ice, add soda water to fill",
-		Cherry: false,
-		Olive: false,
-		Mint: true,
-		Glass: "Highball",
-		Ice: true,
-	},
-	{
-		Name: "Tom Collins",
-		Ingredients: [
-			"Gin - 2oz",
-			"Lemon Juice - 1oz",
-			"Simple Syrup - 0.5oz",
-			"Soda Water - 2oz",
-			"Lemon slice"
-		],
-		Garnish: "Lemon Slice",
-		Directions: "Add all ingredients into glass with ice and stir, add soda water to fill, add lemon slice and cherry",
-		Cherry: true,
-		Olive: false,
-		Mint: false,
-		Glass: "Highball",
-		Ice: true,
-	},
-	{
-		Name: "Gin and Tonic",
-		Ingredients: [
-			"Gin - 2oz",
-			"Tonic Water - 4oz",
-			"Ice to fill",
-			"Lime slice"
-		],
-		Garnish: "Lime Slice",
-		Directions: "Add ice and gin to glass, fill to top with tonic water, stir, add lime to garnish",
-		Cherry: false,
-		Olive: false,
-		Mint: false,
-		Glass: "Highball",
-		Ice: true,
-	},
-	{
-		Name: "Screwdriver",
-		Ingredients: [
-			"Vodka - 2oz",
-			"Orange Juice - 4oz",
-			"Ice to fill",
-			"Orange slice"
-		],
-		Garnish: "Orange Slice",
-		Directions: "Add vodka and orange juice to glass, stir, add ice to fill",
-		Cherry: false,
-		Olive: false,
-		Glass: "Highball",
-		Ice: true,
-	},
-	
-	{
-		Name: "Long Island Iced Tea",
-		Ingredients: [
-			"Vodka - .75oz",
-			"White Rum - .75oz",
-			"Silver Tequila - .75oz",
-			"Gin - .75oz",
-			"Triple Sec - .75oz",
-			"Lemon Juice - 0.5oz",
-			"Cola - 4oz",
-			"Ice to fill",
-			"Lemon slice"
-		],
-		Garnish: "Lemon slice",
-		Cherry: false,
-		Olive: false,
-		Directions: "Add all ingredients, stir, add ice to fill",
-		Glass: "Highball",
-		Ice: true,
-	},
-	
-	{
-		Name: "Whiskey Sour",
-		Ingredients: [
-			"Whiskey - 1.5oz",
-			"Lemon Juice - 1oz",
-			"Simple Syrup - 0.75oz",
-			"Orange slice",
-			"Cherry"
-		],
-		Garnish: "Orange slice",
-		Cherry: true,
-		Olive: false,
-		Directions: "Add whiskey, lemon juice, and simple syrup to glass, shake until chilled. Fill glass with ice cubes, add whiskey and cherry and orange slice garnish",
-		Glass: "Rocks",
-		Ice: true,
-	},
-	
-	{
-		Name: "Old Fashioned",
-		Ingredients: [
-			"Whiskey - 1.5oz",
-			"1 Sugar cube",
-			"Bitters - 2 dashes",
-			"Water - 2 dashes",
-			"Orange slice",
-			"Cherry"
-		],
-		Garnish: "Orange slice",
-		Cherry: true,
-		Olive: false,
-		Directions: "Add sugar cube to glass, soak with bitters, add water, muddle. Fill glass with ice cubes, add whiskey and cherry and orange slice garnish",
-		Glass: "Rocks",
-		Ice: true,
-	},
-	
-	{
-		Name: "White Russian",
-		Ingredients: [
-			"Vodka - 2oz",
-			"Kahlua - 1oz",
-			"Cream - 1oz"
-		],
-		Garnish: "",
-		Cherry: false,
-		Olive: false,
-		Directions: "Stir vodka and Kahlua into glass over ice, float cream on top",
-		Glass: "Rocks",
-		Ice: true,
-	},
-	{
-		Name: "Negroni",
-		Ingredients: [
-			"Gin - 1oz",
-			"Campari - 1oz",
-			"Red Vermouth - 1oz"
-		],
-		Garnish: "Orange slice",
-		Cherry: false,
-		Olive: false,
-		Directions: "Stir into glass over ice, add orange slice garnish",
-		Glass: "Rocks",
-		Ice: true,
-	},
-
-]
 var RecipeManager = {
 	
 	Init: function (){
 		
 		RecipeManager.BindFunctions();
 		console.log("did this here");
-		
+		document.head.insertAdjacentHTML( 'beforeend', RecipeManager.StyleTag.replace(/[\r\n\t]/g, ""));
 	},
 	
 	LiquidParts: 32,
@@ -361,7 +130,101 @@ var RecipeManager = {
 	
 	BindFunctions: function(){
 		
-	}
+	},
+	
+	StyleTag: `<style id='recipeManagerStyle'>
+			#recipeDiv,#recipeMenu {
+			background: black;
+			color: white;
+			font-size: 25px;
+			font-family: Arial;
+		}
+		#recipeDiv {
+			width: 50vw;
+		}
+		#Glass {
+			fill: white !important;
+			stroke: black !important;
+			stroke-width: 3 !important;
+		}
+		#glassDiv,#recipeMenu,#recipeDiv {
+			display: inline-block;
+		}
+		#ingredientsDiv {
+			display: inline-block;
+			vertical-align: top;
+		}
+		span.ingredient-key-color {
+			min-width: 50px !important;
+			display: inline-block;
+		}
+		.ingredient {
+			padding: 5px;
+
+		}
+		span.ingredient-name {
+			padding-left: 10px;
+		}
+		span#ingredientsSpan {
+			margin-top: 50px;
+			display: inline-block;
+		}
+		#recipeDiv {
+			margin: 20px;
+		}
+		[id*='Liquid-']{
+			stroke-width: 1 !important;
+		}
+		#recipeMenu {
+			vertical-align: top;
+			padding-right: 50px;
+			float: right;
+			overflow-y: scroll;
+			height: 600px;
+			margin: 20px;
+		}
+		span#ingredientsSpan {
+			font-size: 20px;
+		}
+
+		#recipeMenu > [id*='glassDiv'] > svg:hover{
+			background: #404040;
+			border-radius: 10px;
+			cursor: pointer;
+		}
+		#recipeMenu > [id*='glassDiv'] > svg:active{
+			background: gray;
+
+		}
+		#Ice-cube-1,#Ice-cube-2{
+			stroke-width: 2 !important;
+			opacity: 0.8 !important;
+		}
+
+		/* width */
+		::-webkit-scrollbar {
+		  width: 8px;
+		}
+
+		/* Track */
+		::-webkit-scrollbar-track {
+		  background: #f1f1f1;
+		  border-radius: 4px;
+		  opacity: 0.5;
+		}
+
+		/* Handle */
+		::-webkit-scrollbar-thumb {
+		  background: #888;
+		  border-radius: 4px;
+		}
+
+		/* Handle on hover */
+		::-webkit-scrollbar-thumb:hover {
+		  background: #555;
+		}
+	
+	</style>`
 	
 }
 RecipeManager.Init();
