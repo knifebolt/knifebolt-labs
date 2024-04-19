6f070accccb1cbe5ee0fdf781c5352e947f291c2
knifebolt <jbaugher@rocketmail.com>
2024-04-18 21:54:48 -0400

    Add files via upload

---
+++ JokesJS/Jokes.js
@@ -0,0 +1,288 @@
+var Jokes = [ 
+	{
+	"Setup":"I went to a museum with a pair of magnetic handcuffs",
+	"Punchline":"It was a two-wrist attraction"
+
+},{	"Setup":"How do sprinters like their eggs?",
+	"Punchline":"Runny"
+
+},{	"Setup":"Lance is an uncommon name now",
+	"Punchline":"But people used to be named Lance a lot"
+
+},{	"Setup":"A priest, a pastor, and a rabbit donate blood",
+	"Punchline":"The rabbit says, I think I'm a type O",
+
+},{	"Setup":"I went to a haunted French restaurant",
+	"Punchline":"It was full of escarghosts"
+
+},{	"Setup":"I tried to look through the holes of a colander",
+	"Punchline":"But I ended up straining my eyes"
+
+},{	"Setup":"I'm missing the main ingredient for an omelette",
+	"Punchline":"I'm having an eggs-essential crisis"
+
+},{	"Setup":"I tried to figure out which wave would hit the shore first",
+	"Punchline":"But they tide"
+
+},{	"Setup":"I had my left leg up in the air at midnight",
+	"Punchline":"I wanted to start 2024 off on the right foot"
+
+},{	"Setup":"I met a guitarist with writer's block",
+	"Punchline":"He had nothing to fret about"
+
+},{	"Setup":"What nationality is Santa Claus?",
+	"Punchline":"North Polish"
+
+},{	"Setup":"I like to give abacus beads as a present",
+	"Punchline":"It's the little things that count"
+	
+},{	"Setup":"You can't order a fountain drink larger than 8 ounces in St. Paul",
+	"Punchline":"It's mini-soda law"
+
+},{	"Setup":"Never invest in a company that makes skis",
+	"Punchline":"The whole sport is going downhill fast"
+
+},{	"Setup":"What's antique wax?",
+	"Punchline":"It sounds like a strong duck repellent"
+
+},{	"Setup":"After I sold my rearview mirror",
+	"Punchline":"I never looked back"
+
+},{	"Setup":"In my home country, people fight to the death all the time",
+	"Punchline":"I have duel citizenship"
+
+},{	"Setup":"How do you cut a group of musicians in half?",
+	"Punchline":"With a band saw"
+
+},{	"Setup":"The front parts of my feet fell asleep",
+	"Punchline":"They're coma-toes"
+
+},{	"Setup":"I saw a pirate wearing a paper towel as a hat",
+	"Punchline":"He had a Bounty on his head"
+
+},{	"Setup":"I saw a magician driving a tractor",
+	"Punchline":"He turned it into a field"
+
+},{	"Setup":"The only way to kill a French vampire is with a baguette",
+	"Punchline":"It's a pain-staking process"
+
+},{	"Setup":"A mime looked like he wanted to tell me something",
+	"Punchline":"But it wasn't aloud"
+
+},{	"Setup":"I built a wine cellar with an elevator",
+	"Punchline":"It lifts my spirits"
+
+},{	"Setup":"Almost all small garden statues have pointy red hats",
+	"Punchline":"It's a little gnome fact"
+
+},{	"Setup":"I clean my own house and wash my own dishes",
+	"Punchline":"I'm a self-maid man"
+
+},{	"Setup":"I can't help it that I'm gaining weight",
+	"Punchline":"I've had a lot on my plate lately"
+
+},{	"Setup":"Saw a girl lining her dolls up on top of a grill",
+	"Punchline":"It was a Barbie queue"
+
+},{	"Setup":"I heard about these spies who planted microphones in a box of tic-tacs",
+	"Punchline":"They were in four mints"
+
+},{	"Setup":"I'm going to teach people about dried out grapes",
+	"Punchline":"It's all about raisin awareness"
+
+},{	"Setup":"My banker doesn't have any friends",
+	"Punchline":"He's too much of a loaner"
+
+},{	"Setup":"They didn't believe I was wearing a harp costume",
+	"Punchline":"I think they were calling me a lyre"
+
+},{	"Setup":"Trees don't have teeth",
+	"Punchline":"They're all bark and no bite"
+
+},{	"Setup":"Ducks keep attacking my dog",
+	"Punchline":"Because he's a pure-bread"
+
+},{	"Setup":"How do communists measure time?",
+	"Punchline":"With an our-glass"
+
+},{	"Setup":"I can't move a chicken coop by myself",
+	"Punchline":"It's too heavy of a bird den"
+
+},{	"Setup":"The Lord of the Rings pinball machine doesn't take quarters",
+	"Punchline":"Only Tolkiens"
+
+},{	"Setup":"The police figured out what the murder weapon was pretty quickly",
+	"Punchline":"It was a brief case"
+
+},{	"Setup":"What's the most famous rock group in American history?",
+	"Punchline":"Mount Rushmore"
+
+},{	"Setup":"I don't trust people who swim every day",
+	"Punchline":"They seem a little fishy to me"
+
+},{	"Setup":"They say the part of the hospital with the least privacy",
+	"Punchline":"Is the ICU"
+
+},{	"Setup":"How do you make Holy Water?",
+	"Punchline":"You take regular water and boil the hell out of it"
+
+},{	"Setup":"I asked the doctor if I could do my own anesthesia",
+	"Punchline":"He said \"Sure, knock yourself out\""
+
+},{	"Setup":"There are no losers in a hot dog eating contest",
+	"Punchline":"Only wieners"
+
+},{	"Setup":"What's a dragon's favorite snack?",
+	"Punchline":"Fire crackers"
+
+},{	"Setup":"I'm gonna buy a race car bed",
+	"Punchline":"Soon I'll be fast asleep"
+
+},{	"Setup":"Stop telling tree jokes?",
+	"Punchline":"You're not the balsa me."
+
+},{	"Setup":"We need more books in prison libraries",
+	"Punchline":"The prose will outweigh the cons"
+
+},{	"Setup":"My tailor also runs a moving service",
+	"Punchline":"He's always hemming and hauling"
+
+},{	"Setup":"I didn't mean to glue my autobiography to my hand",
+	"Punchline":"That's my story and I'm sticking to it"
+
+},{	"Setup":"I trained a dog to bring me a glass of wine",
+	"Punchline":"He's a bordeaux collie"
+
+},{	"Setup":"You shouldn't joke about someone playing the tuba",
+	"Punchline":"It's a low blow"
+
+},{	"Setup":"I want to sell these gloves because they are too tight",
+	"Punchline":"Can someone take them off my hands?"
+
+},{	"Setup":"Did you know Darth Vader had a sister?",
+	"Punchline":"Her name was Ella"
+
+},{	"Setup":"What's one foot long and slippery?",
+	"Punchline":"A slipper"
+
+},{	"Setup":"The first time I saw an escalator",
+	"Punchline":"I stopped and staired"
+
+},{	"Setup":"It's not a law that dudes have to eat together",
+	"Punchline":"That's a mandate"
+
+},{	"Setup":"If my current career doesn't work out I'm gonna become a honey farmer",
+	"Punchline":"It's my Plan Bee"
+
+},{	"Setup":"I only buy corn from pirates.",
+	"Punchline":"I always pay a buccaneer"
+
+},{	"Setup":"Why did the archaeopteryx get the worm?",
+	"Punchline":"Because it's an early bird"
+
+},{	"Setup":"I got an email explaining how to read maps backwards",
+	"Punchline":"It was spam"
+
+},{	"Setup":"For the next 40 days, I'm giving up Word docs in favor of spreadsheets",
+	"Punchline":"It's gonna be Excel lent"
+
+},{	"Setup":"Some people are unsure about driving transparent cars",
+	"Punchline":"I will be steering clear"
+
+},{	"Setup":"I want to get better at bowling",
+	"Punchline":"But I don't have any good roll models"
+
+},{	"Setup":"Dr. Pepper doesn't hate society",
+	"Punchline":"He actually loves pop culture"
+
+},{	"Setup":"Did you hear about the sick juggler?",
+	"Punchline":"He couldn't stop throwing up"
+
+},{	"Setup":"Ducks don't like being stressed out",
+	"Punchline":"They quack under pressure"
+
+},{	"Setup":"Which side of a sheep has the most wool?",
+	"Punchline":"The outside"
+
+},{	"Setup":"You shouldn't eat tin foil",
+	"Punchline":"You might sheet metal"
+
+},{	"Setup":"Ghosts don't like rain",
+	"Punchline":"It dampens their spirits"
+
+},{	"Setup":"Saw a sign that said 'Falling Rocks'",
+	"Punchline":"It doesn't"
+
+},{	"Setup":"The Australians cheered when I brought out dessert",
+	"Punchline":"Which is weird, they usually boo meringue"
+
+},{	"Setup":"You don't audition to wear Iron Man's armor",
+	"Punchline":"They cast you"
+
+},{	"Setup":"Can't believe someone stole my limbo bar",
+	"Punchline":"I mean how low can you get"
+
+},{	"Setup":"My boss thinks it's weird I'm only sick Monday through Friday",
+	"Punchline":"But I have a weekend immune system"
+
+},{	"Setup":"I told the manufacturer my air mattress is lumpy",
+	"Punchline":"They said I'm blowing it out of proportion"
+
+},{	"Setup":"The only thing Flat Earthers fear",
+	"Punchline":"Is sphere itself"
+
+},{	"Setup":"Gonna start a goth dating app called Graveyard",
+	"Punchline":"Instead of liking people, you dig them"
+
+},{	"Setup":"When I was in high school I ran a maratho",
+	"Punchline":"I never finished it"
+
+},{	"Setup":"I asked the bartender for something harder than water",
+	"Punchline":"He gave me ice"
+
+},{	"Setup":"The only way to prove you have de-feeted your enemy",
+	"Punchline":"Is to take a foe-toe"
+
+},{	"Setup":"I thought my shadow puppet theatre would make a fortune",
+	"Punchline":"Had too much faith in projected figures"
+
+},{	"Setup":"Today's my first day working at the spice factory",
+	"Punchline":"It's a seasonal job"
+
+},{	"Setup":"I built a car out of old washing machines",
+	"Punchline":"Can't wait to take it for a spin"
+
+},{	"Setup":"The first three Roman poisons will kill you",
+	"Punchline":"The fourth one, Poison IV, just makes you itchy"
+
+},{	"Setup":"The triangle is my least favorite instrument",
+	"Punchline":"It's just one ting after another"
+
+},{	"Setup":"I'm nervous about wearing this wool sweater in public",
+	"Punchline":"It makes me feel sheepish"
+
+},{	"Setup":"I broke a leg during my audition",
+	"Punchline":"But I still ended up in the cast"
+
+},{	"Setup":"A termite walks up to the counter of a saloon and asks",
+	"Punchline":"'Where\'s the bar tender?'"
+
+},{	"Setup":"My friend got a huge water bill",
+	"Punchline":"I sent him a 'get well soon' card"
+
+},{	"Setup":"Not sure who ripped the final pages out of my comic books",
+	"Punchline":"But I'm drawing my own conclusions"
+
+},{	"Setup":"I love helium",
+	"Punchline":"I can't speak highly enough about it"
+
+},{	"Setup":"I wanted to be an astronaut",
+	"Punchline":"But my parents told me the sky's the limit"
+
+},{	"Setup":"I attempted to list out my favorite knots",
+	"Punchline":"But they were all tied"
+
+},{	"Setup":"You can hear blood in your veins",
+	"Punchline":"If you listen varicosely"
+	}
+]
