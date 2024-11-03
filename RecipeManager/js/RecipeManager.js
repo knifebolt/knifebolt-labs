571ce342027ac9f7d507ffa9c08cb8faae1e9d5f
knifebolt <jbaugher@rocketmail.com>
2024-11-03 17:43:09 -0500

    Update RecipeManager.js

--- RecipeManager/js/RecipeManager.js
+++ RecipeManager/js/RecipeManager.js
@@ -81,8 +81,8 @@ var RecipeManager = {
 		
 		
 		
-		if (recipe.Glass"Glass stemmed mug" ||
-			recipe.Glass"Clay mug" 
+		if (recipe.Glass =="Glass stemmed mug" ||
+			recipe.Glass == "Clay mug" 
 		){
 			$(glassDivSelector).append(Glasses[recipe.Glass]);
 		} 
