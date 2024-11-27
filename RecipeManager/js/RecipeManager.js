4d1a2108a7f0acbe2003b190c6e0e5ae2c14edd5
knifebolt <jbaugher@rocketmail.com>
2024-11-26 21:47:25 -0500

    Update RecipeManager.js

--- RecipeManager/js/RecipeManager.js
+++ RecipeManager/js/RecipeManager.js
@@ -88,7 +88,8 @@ var RecipeManager = {
 			recipe.Glass =="Collins" || 
 		    	recipe.Glass =="Nick and Nora" ||
 		    	recipe.Glass =="Margarita" ||
-		    	recipe.Glass =="Hurricane"
+		    	recipe.Glass =="Hurricane" ||
+		    	recipe.Glass =="Flute"
 		){
 			$(glassDivSelector).append(Glasses[recipe.Glass]);
 		} else if (recipe.Glass =="Martini" ||
