998abd0e42df0cb1e7b942112ee17888305f0f0d
knifebolt <jbaugher@rocketmail.com>
2024-12-01 22:38:04 -0500

    Update RecipeManager.js

--- RecipeManager/js/RecipeManager.js
+++ RecipeManager/js/RecipeManager.js
@@ -244,7 +244,7 @@ var RecipeManager = {
 				garnish =  Garnishes[g];
 			}
 		}
-		if (garnish["Color Change"] != undefined && garnish["Color Change"] != "" && garnish["Color Change"].indexOf(",") > -1){
+		if (garnish["Color Change"] != undefined && garnish["Color Change"] != "" && garnish["Color Change"].indexOf("|") > -1){
 			var garnishSelector = garnish["Color Change"].split('|')[0];
 			var garnishNewColor = garnish["Color Change"].split('|')[1];
 			$(glassDivSelector).find(garnishSelector).css("fill",garnishNewColor).css("stroke",garnishNewColor);
